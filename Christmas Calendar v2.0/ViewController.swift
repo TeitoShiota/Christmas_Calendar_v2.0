9c8514ef48774259d8171303658c3ded2cb3014d
Lasse Karahan Kristiansen <lasselaflair@gmail.com>
2020-11-02 13:19:49 +0100

    Initial Commit

---
+++ Christmas Calendar v2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Christmas Calendar v2.0
+//
+//  Created by Elev GF2 on 02/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
